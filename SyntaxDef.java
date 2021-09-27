d431ee3fa1b695287470783e44e1e734d0411ec1
surendhiran13 <91453483+surendhiran13@users.noreply.github.com>
2021-09-27 13:52:17 +0530

    Add files via upload

---
+++ SyntaxDef.java
@@ -0,0 +1,15 @@
+package progStructure;
+
+public class SyntaxDef {
+	//public-access modifier--to set the access level of the data
+	public static void main(String[] args) {
+	//static-to break dead lock of objects
+	//void-do not return value(null)
+	//main-initial point of the program
+    //string-class(string)
+	//[]-Array
+    //args-arguments
+	//(String[]args)-arrays contains n number of arguments stored in the syntax
+		System.out.println("hello world");
+	}
+}
