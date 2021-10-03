87b776fffbe327d133af08d4c4c645b15e92116f
surendhiran13 <91453483+surendhiran13@users.noreply.github.com>
2021-10-03 22:37:17 +0530

    Add files via upload

---
+++ EqualOperator.java
@@ -0,0 +1,11 @@
+package assingment;
+
+public class EqualOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int x=3;//equal operator in assingment operators
+    System.out.println(x);
+	}
+
+}
